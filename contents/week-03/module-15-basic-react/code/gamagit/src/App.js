88f53480d7839f1a3dff0abf9bb96be2235baa1a
CesarCanoff <canoff.cesar@gmail.com>
2022-06-02 02:46:40 -0400

    Routes Import

--- contents/week-03/module-15-basic-react/code/gamagit/src/App.js
+++ contents/week-03/module-15-basic-react/code/gamagit/src/App.js
@@ -1,25 +1,9 @@
-import React, { useState } from "react";
-import axios from 'axios';
+import React from "react";
+import Routes from "./routes";
 
-function App(props) {
-  const [user, setUser] = useState("");
-  function handleSearch() {
-    axios.get(`https://api.github.com/users/${user}/repos`)
-    .then(response => console.log(response.data))
-  }
+function App() {
   return (
-    <>
-      <p>{user}</p>
-      <input
-        className="user"
-        placeholder="Username"
-        value={user}
-        onChange={(e) => setUser(e.target.value)}
-      />
-      <button onClick={handleSearch} type="button">
-        Search
-      </button>
-    </>
+    <Routes />
   );
 }
 
