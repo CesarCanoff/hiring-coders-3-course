98681ceedc9f7fb4f552937512f059cc030e9ce8
CesarCanoff <canoff.cesar@gmail.com>
2022-06-02 02:30:47 -0400

    Adding React Router Dom

--- contents/week-03/module-15-basic-react/code/gamagit/src/index.js
+++ contents/week-03/module-15-basic-react/code/gamagit/src/index.js
@@ -8,4 +8,3 @@ root.render(
     <App title='Hello, Men!' />
   </div>
 );
-
---
+++ contents/week-03/module-15-basic-react/code/gamagit/src/routes.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import Repositories from "./Repositories";
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/repositories" component={Repositories} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
