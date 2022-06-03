e0d5bce9b34d9686aca6647d33150f03f6004d08
CesarCanoff <canoff.cesar@gmail.com>
2022-06-03 17:31:55 -0400

    Creating Server and Routes :)

--- contents/week-03/module-17-graphql/code/src/main.js
+++ contents/week-03/module-17-graphql/code/src/main.js
@@ -1,1 +1,29 @@
-console.log('Hello Sir!');
+import { createServer } from "http";
+
+const server = createServer((request, response) => {
+  switch (request.url) {
+    case "/status": {
+      response.writeHead(200, {
+        "Content-Type": "application/json",
+      });
+      response.write(
+        JSON.stringify({
+          status: "OK",
+        })
+      );
+      response.end();
+      break;
+    }
+    default: {
+      response.writeHead(404, "Service not found!");
+      response.end();
+    }
+  }
+});
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : "3000";
+const HOSTNAME = process.env.HOSTNAME || "127.0.0.1";
+
+server.listen(PORT, HOSTNAME, () => {
+  console.log(`Server is running at http://${HOSTNAME}:${PORT}`);
+});
