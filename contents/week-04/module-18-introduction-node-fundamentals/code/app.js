ad805a0e34743f5b211459a1fa7e660a29fdad6e
CesarCanoff <canoff.cesar@gmail.com>
2022-06-06 18:58:33 -0400

    First 'Hello, World' at NodeJS of Gama Academy Course | Creating Server

---
+++ contents/week-04/module-18-introduction-node-fundamentals/code/app.js
@@ -0,0 +1,18 @@
+const http = require("http");
+
+// CREATING VARIABLES.
+const HOSTNAME = "127.0.0.1";
+const PORT = 3000;
+
+// Criando o servidor.
+const server = http.createServer((request, response) => {
+  response.statusCode = 200; // Status de retorno de sucesso.
+  response.setHeader("Content-Type", "text/plain");
+  response.end("Hello, World!"); // Renderizando a mensagem na tela.
+});
+
+// Servidor "ouvindo" na porta 3000.
+server.listen(PORT, HOSTNAME, () => {
+  console.log(`Server is running at http://${HOSTNAME}:${PORT}`);
+  // Nome do host e porta sendo carregados por variáveis [Template String]. 
+});
---
+++ contents/week-04/module-18-introduction-node-fundamentals/code/nodejs-guide.md
@@ -0,0 +1,4 @@
+# NodeJS
+
+## Steps
+- 
