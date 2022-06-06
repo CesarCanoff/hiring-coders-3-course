b4de7447c20da477ba4c1045295bcf5ed37d1763
CesarCanoff <canoff.cesar@gmail.com>
2022-06-06 13:33:21 -0400

    Loading Sign-In Page

--- contents/week-03/module-17-graphql/code/src/main.js
+++ contents/week-03/module-17-graphql/code/src/main.js
@@ -1,4 +1,6 @@
 import { createServer } from "http";
+import { readFile } from "fs";
+import { resolve } from "path";
 
 const server = createServer((request, response) => {
   switch (request.url) {
@@ -12,6 +14,23 @@ const server = createServer((request, response) => {
         })
       );
       response.end();
+      break;
+    }
+    case "/sign-in": {
+      const path = resolve(__dirname, "./pages/sign-in.html");
+      readFile(path, (error, file) => {
+        if (error) {
+          response.writeHead(500, "Can't process HTML file.");
+          response.end();
+          return;
+        }
+
+        response.writeHead(200);
+        response.write(file);
+        response.end();
+      });
+    }
+    case "/authenticate": {
       break;
     }
     default: {
