eea6c44fe5159fd563313bfc8335242d683076b2
CesarCanoff <canoff.cesar@gmail.com>
2022-06-09 11:15:53 -0400

    Feature: Implements search user with output in JSON

--- contents/week-04/module-18-introduction-node-fundamentals/code/crud-nodejs/app.js
+++ contents/week-04/module-18-introduction-node-fundamentals/code/crud-nodejs/app.js
@@ -7,33 +7,36 @@ const HOSTNAME = "127.0.0.1";
 const PORT = 3000;
 
 const server = http.createServer((request, response) => {
-  const params = queryString.parse(url.parse(request.url, true).search);
-  let responseHTML;
-  // let userName = params.name;
-  // let idUser = params.id;
+  const urlParse = url.parse(request.url, true);
+  const params = queryString.parse(urlParse.search);
+  var responseHTML;
 
-  fs.writeFile(
-    `./users/${params.id}.txt`,
-    `${JSON.stringify(params)}`,
-    (error) => {
+  if (urlParse.pathname === "/create-user") {
+    fs.writeFile(
+      `./users/${params.id}.txt`,
+      `${JSON.stringify(params)}`,
+      (error) => {
+        if (error) throw error;
+
+        response.statusCode = 201;
+        response.setHeader("Content-Type", "text/plain");
+        response.end(responseHTML);
+      }
+    );
+
+    responseHTML = "User was saved successfully.";
+  } else if (urlParse.pathname === "/search-user") {
+    fs.readFile(`./users/${params.id}.txt`, (error, data) => {
       if (error) throw error;
-    }
-  );
+      responseHTML = data;
 
-  responseHTML = "User was saved successfully.";
-
-  response.statusCode = 200;
-  response.setHeader("Content-Type", "text/plain");
-  response.end(responseHTML);
+      response.statusCode = 200;
+      response.setHeader("Content-Type", "application/json");
+      response.end(responseHTML);
+    });
+  }
 });
 
 server.listen(PORT, HOSTNAME, () => {
   console.log(`Server is running at http://${HOSTNAME}:${PORT}`);
 });
-
-/**
- * 1 - Create user;
- * 2 - Read/Select/Search user;
- * 3 - Update user;
- * 4 - Delete user;
- */
