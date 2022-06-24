9f00a9870ed1a39a7c0992e344ec5b7f0add7171
cesarcanoff <canoff.cesar@gmail.com>
2022-06-24 00:03:06 -0400

    Feat: Creating News Search Service

--- contents/week-06/code/news-api/index.js
+++ contents/week-06/code/news-api/index.js
@@ -19,26 +19,37 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-
 // Rota principal - (index).
 app.get("/", (request, response) => {
   response.send("Hello, World!");
 });
 
 app.get("/news-api/v1/categorias", (request, response) => {
-  connection.query("SELECT id, nome FROM sistema_noticias.categoria", function (err, rows, fields) {
-    if (err) throw err;
-    
-    response.send(rows);
-  });
+  connection.query(
+    "SELECT id, nome FROM sistema_noticias.categoria",
+    function (err, rows, fields) {
+      if (err) throw err;
+
+      response.send(rows);
+    }
+  );
   connection.end();
-  
 });
 
 // Serviço de busca de notícias.
-app.get('/news-api/v1/categorias/:categoriaId/noticias', (request, response) => {
-  response.send(request.params.categoriaId);
-});
+app.get(
+  "/news-api/v1/categorias/:categoriaId/noticias",
+  (request, response) => {
+    connection.query(
+      'SELECT id, titulo FROM sistema_noticias.noticia WHERE id_categoria = ' + request.params.categoriaId,
+      function (err, rows, fields) {
+        if (err) throw err;
+
+        response.send(rows);
+      }
+    );
+  }
+);
 
 // Servidor rodando.
 app.listen(port, () => {
