84bde4e26369e825bbb9c250d23c7da31da4694f
cesarcanoff <canoff.cesar@gmail.com>
2022-06-24 15:11:29 -0400

    Node-Fetch 2/3

--- contents/week-06/module-21-third-party-api- consumption/code/lib/cielo.js
+++ contents/week-06/module-21-third-party-api- consumption/code/lib/cielo.js
@@ -1,7 +1,19 @@
 class Cielo {
   static compra(params) {
-    return params;
-  }
+    var fetch  = require('node-fetch');
+    
+    const response = fetch('https://apisandbox.cieloecommerce.cielo.com.br/1/sales/', {
+      method: 'post',
+      body: JSON.stringify(params),
+      headers: {
+        'Content-Type': 'application/json',
+        'MerchantId': '1377ee0d-cfbc-4d3f-9105-38b527205986',
+        'MerchantKey': 'RJGWKLEOXAHIIYKGQYFIKJZYLAMXPLJCRZAKFGYO'
+      }
+    });
+    const data = response.json();
+    
+    console.log(data);
 }
-
+}
 module.exports = Cielo;
