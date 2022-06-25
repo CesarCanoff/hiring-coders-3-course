af43a232fa8ba59b7e17d9eb5b994cfc5fae9ee8
cesarcanoff <canoff.cesar@gmail.com>
2022-06-25 16:10:54 -0400

    Feat: Categories file and api connect

--- contents/week-07/code/aula-nextjs/pages/categories.tsx
+++ contents/week-07/code/aula-nextjs/pages/categories.tsx
@@ -1,5 +1,43 @@
-import React from 'react';
-import { GetStaticProps } from 'next';
+import React from "react";
+import { GetStaticProps, GetStaticProps } from "next";
 
-import api from '../service/apiInterna.json';
+import api from "../service/apiInterna.json";
 
+import { Container } from "../styles/Dashboard/styles";
+
+interface ICategories {
+  id: number;
+  title: string;
+}
+
+interface CategoryProps {
+  categories: ICategories[];
+}
+
+export default function categories({ categories }: CategoryProps) {
+  return (
+    <Container>
+      <h1>Categorias</h1>
+      <section>
+        <ul>
+          {categories.map((category) => {
+            return <li key={category.id}>{category.title}</li>;
+          })}
+        </ul>
+      </section>
+    </Container>
+  );
+}
+
+export const getStaticProps: GetStaticProps<CategoryProps> = async () => {
+  const response = await api.get("http://localhost:3333/categories");
+
+  const categories = await response.data
+
+  return {
+    props: {
+      categories,
+    },
+    revalidate: 5,
+  };
+};
