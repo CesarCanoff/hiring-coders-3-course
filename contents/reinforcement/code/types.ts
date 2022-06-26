4a808f37bd468216a370f55636ba3db1b1bb8c68
cesarcanoff <canoff.cesar@gmail.com>
2022-06-26 19:55:57 -0400

    Typescript Types I

---
+++ contents/reinforcement/code/types.ts
@@ -0,0 +1,56 @@
+// Boolean
+const isPerson: boolean = false;
+// ==============================
+
+// Number
+const age: number = 18;
+const height: number = 1.8;
+// ==============================
+
+// String
+const fullName: string = "César Canoff";
+// ==============================
+
+// Array
+const ages: number[] = [23, 14, 96, 55];
+// ==============================
+
+// Tuple - Three values
+let players: [string, string, string];
+players = ["Canoff", "César", "Caio"];
+// ==============================
+
+// Enum
+enum StatusAprovacao {
+  Aprovado = "001",
+  Pendente = "002",
+  Reprovado = "003",
+}
+
+// Recebe "APROVADO"
+const statusDaAprovacao: StatusAprovacao = StatusAprovacao.Aprovado;
+// ==============================
+
+// Any
+const apiReturn: any[] = [123, "Bull", true];
+const apiReturn_2: any = {};
+// ==============================
+
+// Void
+function printScreen(msg: string): void {
+  console.log(msg);
+}
+// ==============================
+
+// Null e Undefined
+const u: undefined = undefined;
+const n: null = null;
+// ==============================
+
+// Object
+function criar(obj: object) {
+  // ...
+}
+
+criar({});
+// ==============================
