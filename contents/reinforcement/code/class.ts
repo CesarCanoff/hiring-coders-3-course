beea1f047c4a1e552e18ca90e3900c2964d13e36
cesarcanoff <canoff.cesar@gmail.com>
2022-06-26 21:41:35 -0400

    Typescript Access Modifiers

--- contents/reinforcement/code/class.ts
+++ contents/reinforcement/code/class.ts
@@ -16,3 +16,27 @@ const data = new Data(1, 2);
 console.log(data.day);
 console.log(data.month);
 console.log(data.year);
+
+class Car {
+  private velocidadeAtual: number = 0;
+
+  constructor(
+    public marca: string,
+    public modelo: string,
+    private velocidadeMax: number = 220
+  ) {}
+
+  private alterarVelocidade(delta: number) {
+    //...
+  }
+
+  acelerar() {
+    this.alterarVelocidade(5);
+  }
+
+  frear() {
+    this.alterarVelocidade(-5);
+  }
+}
+
+const carro = new Car("Lamborghini", "Urus", 450);
