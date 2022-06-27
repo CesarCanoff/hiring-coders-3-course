7c3b00a86709ce4eefcf4a2156f502ca2298fd90
cesarcanoff <canoff.cesar@gmail.com>
2022-06-26 21:13:32 -0400

    Typescript Interfaces II

---
+++ contents/reinforcement/code/interfaces.ts
@@ -0,0 +1,16 @@
+interface User {
+  name: string;
+  email: string;
+  address?: string;
+}
+
+function getUser(): User {
+  return {
+    name: 'César',
+    email: 'canoff@gmail.com'
+  }
+}
+
+function setUser(user: User) {
+  //...
+}
