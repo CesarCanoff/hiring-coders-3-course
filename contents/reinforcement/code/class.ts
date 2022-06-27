b18d641f6a3e9f62a9687ceaa02887045b37176a
cesarcanoff <canoff.cesar@gmail.com>
2022-06-26 21:49:56 -0400

    Typescript Heritage

--- contents/reinforcement/code/class.ts
+++ contents/reinforcement/code/class.ts
@@ -27,7 +27,12 @@ class Car {
   ) {}
 
   private alterarVelocidade(delta: number) {
-    //...
+    const novaVelocidade = this.velocidadeAtual + delta;
+    if (novaVelocidade >= 0 && novaVelocidade <= this.velocidadeMax) {
+      this.velocidadeAtual = novaVelocidade;
+    } else {
+      this.velocidadeAtual = delta > 0 ? this.velocidadeMax : 0;
+    }
   }
 
   acelerar() {
@@ -40,3 +45,21 @@ class Car {
 }
 
 const carro = new Car("Lamborghini", "Urus", 450);
+carro.acelerar();
+
+class Camaro extends Car {
+  private turbo = false;
+
+  constructor() {
+    super("Chevrolet", "Camaro", 500);
+  }
+
+  ligarTurbo() {
+    this.turbo = true;
+  }
+}
+
+const camaro = new Camaro();
+camaro.acelerar();
+camaro.frear();
+camaro.ligarTurbo();
