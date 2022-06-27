334b06ea1af1d5ee29c81de914963ac6eaee45a3
cesarcanoff <canoff.cesar@gmail.com>
2022-06-27 15:33:43 -0400

    Cleaning Project

--- contents/reinforcement/reactjs/gama-react/src/App.tsx
+++ contents/reinforcement/reactjs/gama-react/src/App.tsx
@@ -1,25 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi, I'm César
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Olá</h1>
   );
 }
 
