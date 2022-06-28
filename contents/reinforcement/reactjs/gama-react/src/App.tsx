4d80cefdcf5b0ff73e81e352c2ab428e39e41c71
cesarcanoff <canoff.cesar@gmail.com>
2022-06-27 21:51:16 -0400

    ReactJS Components IV

--- contents/reinforcement/reactjs/gama-react/src/App.tsx
+++ contents/reinforcement/reactjs/gama-react/src/App.tsx
@@ -3,10 +3,15 @@ import Box from "./components/Box";
 import "./styles/global.css";
 
 function App() {
+  const sendEmail = () => {
+    alert('E-mail enviado!');
+  }
+
   return (
     <div className="app">
       <Header />
       <main>
+        <button onClick={sendEmail}>Teste</button>
         <Box background="dark">
           <h2>AAAAAS</h2>
         </Box>
