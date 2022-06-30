f5d971414af07dc66223afdb7f98746c61920638
cesarcanoff <canoff.cesar@gmail.com>
2022-06-30 01:18:08 -0400

    NodeJS - Express I | Routes II

--- contents/reinforcement/express/routes/index.js
+++ contents/reinforcement/express/routes/index.js
@@ -6,6 +6,12 @@ routes.get("/", (req, res) => {
   res.send("Olá Mundo!");
 });
 
+// PEGA ID/CATEGORIA DA URL.
+routes.get("/produtos/:id?/:categoria", (req, res) => {
+  console.log(req.params)
+  res.send("Olá Mundo!");
+});
+
 routes.post("/cadastrar", (req, res) => {
   console.log(req.body);
   res.json(req.body);
