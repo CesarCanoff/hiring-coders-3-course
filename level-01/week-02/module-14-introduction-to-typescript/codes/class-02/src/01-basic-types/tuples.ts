5a182082c0ca87543823998dbce345f3f4f45b88
cesarcanoff <canoff.cesar@gmail.com>
2022-09-03 01:24:09 -0400

    Renaming Directories, Module Documentation, Classes and Files

--- level-01/week-02/module-14-introduction-to-typescript/codes/class-02/src/01-basic-types/tuples.ts
+++
@@ -1,5 +0,0 @@
-const pets: [string, string, string] = ["Qwerty", "Eros", "Boult"];
-const Querty: [string, number] = ["Qwerty", 5];
-const Eros: [string, number] = ["Eros", 7];
-const Boult: [string, number] = ["Boult", 3];
-
